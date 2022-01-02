81027b0ae1cfdd62289ac420c56de346af42b31f
Watcharapong Tassoongnern <brutal99@gmail.com>
2022-01-02 19:34:29 +0700

    Lock and unlock return with error

--- api/mutexpools/Lock.go
+++ api/mutexpools/Lock.go
@@ -1,17 +1,22 @@
 package mutexpools
 
 import (
+	"errors"
+
 	"github.com/SERV4BIZ/gfp/jsons"
 	"github.com/SERV4BIZ/mutexpool/api/networks"
 )
 
 // Write is write file to coresan
-func (me *MutexPoolSession) Lock(txtUID string) bool {
+func (me *MutexPoolSession) Lock(txtUID string) (bool, error) {
 	jsoCmd := new(jsons.JSONObject).Factory()
 	jsoCmd.PutString("txt_command", "lock")
 	jsoCmd.PutString("txt_ruid", me.UUID)
 	jsoCmd.PutString("txt_ouid", txtUID)
 
 	jsoReq := networks.Request(me.MutexPoolHost.JSOConfigHost, jsoCmd)
-	return jsoReq.GetInt("status") > 0
+	if jsoReq.GetInt("status") <= 0 {
+		return false, errors.New(jsoReq.GetString("txt_msg"))
+	}
+	return true, nil
 }
